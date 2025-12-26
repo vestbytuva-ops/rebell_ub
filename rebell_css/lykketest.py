2963a5c970396f0e86081a683af89c4349257076
vestbytuva-ops <vestby.tuva@gmail.com>
2025-12-26 19:48:31 +0100

    Laget modal for produkter slik at man kan se produktene i et større bilde og fikset universell utforming ved å fikse clamp.

---
+++ rebell_css/lykketest.py
@@ -0,0 +1,8 @@
+import random 
+
+navn = ["Casper", "Eric", "Emil", "Matheo", "Lorenzo", "Nikolai", "Hakon"]
+
+employed = random.choices(navn)
+
+print("Det ser ut som denne personen får seg jobb i 2067", employed)
+
---
+++ rebell_js/lykketest.js
@@ -0,0 +1,3 @@
+navn = ["Casper", "Eric", "Emil", "Matheo", "Lorenzo", "Nikolai", "Hakon"];
+employed = random.choices(navn);
+print("Det ser ut som denne personen får seg jobb i 2067", employed);
