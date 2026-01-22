db2f20e1acaf2a7cfed08bb0718217b51a267a83
Emil <emil.a.eri28gmail.com>
2026-01-22 13:04:42 +0100

    Endringer i devside, nesten ferdig

--- rebell_js/testing.js
+++ rebell_js/testing.js
@@ -1,17 +1,3 @@
-let products = []
-
-// Funksjon for å legge til nye elementer
-function addItem(name, category, price) {
-  const newItem = {
-    id: items.length + 1, // enkel id
-    name,
-    category,
-    price
-  };
-  items.push(newItem);
-  console.log(`Lagt til:`, newItem);
-}
-
 // Test firebase
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
@@ -35,7 +21,108 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getDatabase(app);
-
 const productRef = ref(db, 'currentProduct');
 
+
+class ProductManager {
+  constructor() {
+    this.products = this.loadProducts();
+    this.productGrid = document.querySelector('.product-grid');
+    this.init();
+  }
+
+  loadProducts() {
+    onValue(productRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        this.products = Object.keys(data).map(key => ({
+          firebaseId: key,
+          ...data[key]
+        }));
+    } else {
+      this.products = [];
+    }
+    this.renderProducts();
+    });
+  } 
+
+  init() {
+    this.loadProducts ();
+    this.createAdminPanel ();
+  }
+
+  renderProducts() {
+    if (!this.productGrid) return;
+    this.productGrid.innerHTML = '';
+    this.products.forEach(product => {
+      const productCard = this.createProductCard(product);
+      this.productGrid.appendChild(productCard);
+    });
+  }
+
+  createProductCard(product) {
+    const link = document.createElement('a');
+    link.href = product.link;
+    link.className = 'product-link';
+
+    const card = document.createElement('div');
+    card.className = 'product-card';
+    card.dataset.productId = product.firebaseId;
+
+    card.innerHTML = `
+      <img src="${product.image}" alt="${product.name}">
+      <h3>${product.name}</h3>
+      <p>${product.discription}</p>
+      <div class="admin-buttons">
+        <button onclick="productManager.editProduct(${product.id})"</button>
+        <button onclick="productManager.deleteProduct(${product.id})"</button>
+      </div>
+    `;
+
+    link.appendChild(card);
+    return link;
+  }
+
+  addProducts(product) {
+    push(productRef, product);
+    alert('Produkt lagt til i skyen!');
+  }
+
+  editProduct(firebaseId) {
+    const prduct = this.products.find(p => p.firebaseId === firebaseId);
+    if (product) {
+      this.showProductForm(product);
+    } 
+  }
+
+  updateProduct(updateProduct) {
+    const id = updatedProduct.firebaseId;
+    const dataToSave = {...updateProduct };
+    delete dataToSave.firebaseId;
+
+    set(ref(db, `products/${id}`), dataToSave);
+    alert('Produkt oppdatert for alle!');
+  }
+
+  deleteProduct(firebaseId) {
+    if(confirm('Vil du slette dette produktet?')) {
+      remove(ref(db, `products/${firebaseId}`));
+    }
+  }
+
+
+}
+
+
+window.ProductManager = new ProductManager();
+
+
 const inputField = document.getElementById('prduct-input')
+const updateBtn = document.getElementById('update-btn')
+const display = document.getElementById('display')
+
+updateBtn.addEventListener('click', () => {
+  const newName = inputField.value;
+  set(productRef, newName);
+});
+
